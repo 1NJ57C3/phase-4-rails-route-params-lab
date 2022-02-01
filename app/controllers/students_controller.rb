b866e19f8aa7ba9bc6ea905978e0df2eafc8641c
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2022-01-31 21:23:18 -0800

    commit tree 78855126c457e9ce54b326136a0a8093eaa6e34e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = !!params[:name] ? Student.where('first_name LIKE ? OR last_name LIKE ?', params[:name], params[:name]) : Student.all
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
